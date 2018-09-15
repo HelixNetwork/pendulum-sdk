ba8047fc6c700c7a22c7144c1ebda27bd9e56e13
ofo42 <of@hlx.ai>
2018-09-15 17:51:14 +0200

    fix style

--- packages/sha3/test/sha3.absorb-squeeze.test.ts
+++ packages/sha3/test/sha3.absorb-squeeze.test.ts
@@ -1,12 +1,12 @@
-import test from "ava";
-import Sha3 from "../src";
-import { bytes, hex } from "@helixnetwork/converter";
+import test from 'ava'
+import Sha3 from '../src'
+import { bytes, hex } from '@helixnetwork/converter'
 
-test("SHA-3: absorb() and squeeze()", t => {
+test('SHA-3: absorb() and squeeze()', t => {
   const input =
-    "964b398ecd55793d8ca93e01274efe1377a70c8dc358fdca17cb4e94a9ed7777";
+    '964b398ecd55793d8ca93e01274efe1377a70c8dc358fdca17cb4e94a9ed7777'
   const expected =
-    "c2a26a52b0da35baf172d0069cf890f47e351a184b534fffa7d9e4366532d9f9";
+    'c2a26a52b0da35baf172d0069cf890f47e351a184b534fffa7d9e4366532d9f9'
 
   const absorbSqueeze = (input: string): string => {
     const sha3: Sha3 = new Sha3();
@@ -15,11 +15,11 @@ test("SHA-3: absorb() and squeeze()", t => {
     const hash = new Int8Array(Sha3.HASH_LENGTH);
     sha3.squeeze(hash, 0, Sha3.HASH_LENGTH);
     return hex(hash);
-  };
+  }
 
   t.is(
     absorbSqueeze(input),
     expected,
-    "SHA-3 should produce correct hash for absorb/squeeze case."
-  );
-});
+    'SHA-3 should produce correct hash for absorb/squeeze case.'
+  )
+})
