d9d85619eb8de6f7b47e8b245984b50bbe6efbfe
netobjexuser2 <43613212+netobjexuser2@users.noreply.github.com>
2018-11-16 08:43:16 +0200

    Convert http-client package to binary model

--- packages/http-client/test/isBatchableCommand.test.ts
+++ packages/http-client/test/isBatchableCommand.test.ts
@@ -1,25 +1,29 @@
-import { test } from 'ava'
-import { BaseCommand, FindTransactionsCommand, ProtocolCommand } from '../../types'
-import { isBatchableCommand } from '../src/httpClient'
+import { test } from "ava";
+import {
+  BaseCommand,
+  FindTransactionsCommand,
+  ProtocolCommand
+} from "../../types";
+import { isBatchableCommand } from "../src/httpClient";
 
 const batchableCommand: FindTransactionsCommand = {
-    command: ProtocolCommand.FIND_TRANSACTIONS,
-    addresses: ['A'.repeat(81), 'B'.repeat(81)],
-}
+  command: ProtocolCommand.FIND_TRANSACTIONS,
+  addresses: ["a".repeat(2 * 32), "b".repeat(2 * 32)]
+};
 
 interface CustomCommand extends BaseCommand {
-    key: ReadonlyArray<string>
+  key: ReadonlyArray<string>;
 }
 
 const nonBatchableCommand: CustomCommand = {
-    command: 'command',
-    key: ['key'],
-}
+  command: "command",
+  key: ["key"]
+};
 
-test('isBatchableCommand() returns true for batchable commands.', t => {
-    t.is(isBatchableCommand(batchableCommand), true)
-})
+test("isBatchableCommand() returns true for batchable commands.", t => {
+  t.is(isBatchableCommand(batchableCommand), true);
+});
 
-test('isBatchableCommand() returns false for non-batchable commands.', t => {
-    t.is(isBatchableCommand(nonBatchableCommand), false)
-})
+test("isBatchableCommand() returns false for non-batchable commands.", t => {
+  t.is(isBatchableCommand(nonBatchableCommand), false);
+});
