3e442858d19b6525502ba575d2f600a773c7ac91
netobjexuser2 <43613212+netobjexuser2@users.noreply.github.com>
2018-11-29 10:01:17 +0200

    Enable transaction tests

--- packages/validators/test/isTailTransaction.test.ts
+++ packages/validators/test/isTailTransaction.test.ts
@@ -1,19 +1,19 @@
 import test from "ava";
 import { bundle } from "@helix/samples";
 import { isTailTransaction } from "../src";
-// todo check tests
+
 test("isTailTransaction() returns true for valid tail transaction.", t => {
   t.is(
-    true, //isTailTransaction(bundle[0]),
+    isTailTransaction(bundle[0]),
     true,
     "isTailTransaction() should return true for valid tail transaction."
   );
 });
 
-// test("isTailTransaction() returns false for non-tail transaction.", t => {
-//   t.is(
-//     isTailTransaction(bundle[1]),
-//     false,
-//     "isTransactionHash() should return false for non-tail transaction."
-//   );
-// });
+test("isTailTransaction() returns false for non-tail transaction.", t => {
+  t.is(
+    isTailTransaction(bundle[1]),
+    false,
+    "isTransactionHash() should return false for non-tail transaction."
+  );
+});
