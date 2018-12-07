5826f2380f6e214202b330d338d27259a2d5b84b
netobjexuser2 <43613212+netobjexuser2@users.noreply.github.com>
2018-12-07 16:27:54 +0200

    Update test

--- packages/core/test/integration/getBundle.test.ts
+++ packages/core/test/integration/getBundle.test.ts
@@ -15,14 +15,14 @@ test("getBundle() resolves to correct bundle.", async t => {
     "getBundle() should resolve to correct bundle."
   );
 });
-// todo_this : check test
-// test("getBundle() resolves to correct signle transaction bundle.", async t => {
-//   t.deepEqual(
-//     await getBundle(bundleWithZeroValue[0].hash),
-//     bundleWithZeroValue,
-//     "getBundle() should resolve to correct single transaction bundle."
-//   );
-// });
+
+test("getBundle() resolves to correct signle transaction bundle.", async t => {
+  t.deepEqual(
+    await getBundle(bundleWithZeroValue[0].hash),
+    bundleWithZeroValue,
+    "getBundle() should resolve to correct single transaction bundle."
+  );
+});
 
 test("getBundle() rejects with correct error for invalid hash.", t => {
   const invalidHash = "asdasDSFDAFD";
