f44b86e014b97ddc46bcf68c2fb0e7a9b0ad2df0
netobjexuser2 <43613212+netobjexuser2@users.noreply.github.com>
2019-01-23 10:47:33 +0200

    Fixed sendBytes, storeAndBroadcast, traversBundle

--- packages/core/test/integration/sendBytes.test.ts
+++ packages/core/test/integration/sendBytes.test.ts
@@ -12,7 +12,7 @@ const { minWeightMagnitude, hbytes } = attachToTangleCommand;
 const { depth } = getTransactionsToApproveCommand;
 
 const sendHBytes = createSendHBytes(createHttpClient());
-// todo check test
+
 test("sendHBytes() attaches to tangle, broadcasts, stores and resolves to transaction objects.", async t => {
   t.deepEqual(
     await sendHBytes(hbytes, depth, minWeightMagnitude),
@@ -20,47 +20,47 @@ test("sendHBytes() attaches to tangle, broadcasts, stores and resolves to transa
     "sendHBytes() should attach to tangle, broadcast, store and resolve to transaction objects."
   );
 });
-// // todo check test
-// test("sendHBytes() does not mutate original hbytes.", async t => {
-//   const hbytesCopy = [...hbytes];
-//
-//   await sendHBytes(hbytesCopy, depth, minWeightMagnitude);
-//   t.deepEqual(
-//     hbytesCopy,
-//     hbytes,
-//     "sendHBytes() should not mutate original hbytes."
-//   );
-// });
-// // todo check test
-// test("sendHBytes() rejects with correct errors for invalid input.", t => {
-//   const invalidHBytes = ["asdasDSFDAFD"];
-//
-//   t.is(
-//     t.throws(() => sendHBytes(invalidHBytes, depth, minWeightMagnitude), Error)
-//       .message,
-//     `${INVALID_TRANSACTION_HBYTES}: ${invalidHBytes[0]}`,
-//     "sendHBytes() should throw correct error for invalid hbytes."
-//   );
-// });
-//
-// test.cb("sendHBytes() invokes callback", t => {
-//   sendHBytes(hbytes, depth, minWeightMagnitude, undefined, t.end);
-// });
-// // todo check test
-// test.cb("sendHBytes() passes correct arguments to callback", t => {
-//   sendHBytes(hbytes, depth, minWeightMagnitude, undefined, (err, res) => {
-//     t.is(
-//       err,
-//       null,
-//       "sendHBytes() should pass null as first argument in callback for successuful requests"
-//     );
-//
-//     t.deepEqual(
-//       res,
-//       bundle,
-//       "sendHBytes() should pass the correct response as second argument in callback"
-//     );
-//
-//     t.end();
-//   });
-// });
+
+test("sendHBytes() does not mutate original hbytes.", async t => {
+  const hbytesCopy = [...hbytes];
+
+  await sendHBytes(hbytesCopy, depth, minWeightMagnitude);
+  t.deepEqual(
+    hbytesCopy,
+    hbytes,
+    "sendHBytes() should not mutate original hbytes."
+  );
+});
+
+test("sendHBytes() rejects with correct errors for invalid input.", t => {
+  const invalidHBytes = ["asdasDSFDAFD"];
+
+  t.is(
+    t.throws(() => sendHBytes(invalidHBytes, depth, minWeightMagnitude), Error)
+      .message,
+    `${INVALID_TRANSACTION_HBYTES}: ${invalidHBytes[0]}`,
+    "sendHBytes() should throw correct error for invalid hbytes."
+  );
+});
+
+test.cb("sendHBytes() invokes callback", t => {
+  sendHBytes(hbytes, depth, minWeightMagnitude, undefined, t.end);
+});
+
+test.cb("sendHBytes() passes correct arguments to callback", t => {
+  sendHBytes(hbytes, depth, minWeightMagnitude, undefined, (err, res) => {
+    t.is(
+      err,
+      null,
+      "sendHBytes() should pass null as first argument in callback for successuful requests"
+    );
+
+    t.deepEqual(
+      res,
+      bundle,
+      "sendHBytes() should pass the correct response as second argument in callback"
+    );
+
+    t.end();
+  });
+});
