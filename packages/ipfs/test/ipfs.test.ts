4edc25248988d6ce1bc7b921d71b62b2d6fe10c5
ofo42 <of@hlx.ai>
2019-03-02 22:55:43 +0100

    ipfs test incomplete

--- packages/ipfs/test/ipfs.test.ts
+++ packages/ipfs/test/ipfs.test.ts
@@ -1,18 +1,20 @@
-import { hex, toHBytes } from "@helixnetwork/converter";
 import test from "ava";
 import Ipfs from "../src";
 
-/** @todo wip */
-test("Ipfs: pull()/push()", t => {
+/** @todo unit test for ipfs. */
+test("Ipfs: publish()/pull()", t => {
   const input =
     "c02c4aa8852301f3eb7b926f320d911bb178ba1ec4159f67d6cc1d75ef9a62f8";
-  const pushPull = (input: string): string => {
-    let output = input; // pushing and pulling here...
+  const ipfs: Ipfs = new Ipfs("3.122.144.244", "5001");
+  const publishPull = (input: string): string => {
+    // ipfs.publish("c02c4aa8852301f3eb7b926f320d911bb178ba1ec4159f67d6cc1d75ef9a62f8", false);
+    // ipfs.pullMessage("QmWATWQ7fVPP2EFGu71UkfnqhYXDYH566qy47CnJDgvs8u", "./samples/prv-test.pem", false);
+    let output = input;
     return output;
   };
 
   t.is(
-    pushPull(input),
+    publishPull(input),
     input,
     "Pushed message should correspond to pulled message."
   );
