b8865b6aad939ada3e54f48594bde6488ecc979b
ofo42 <of@hlx.ai>
2019-03-02 22:50:05 +0100

    rsa unit test

--- packages/rsa/test/rsa.test.ts
+++ packages/rsa/test/rsa.test.ts
@@ -1,18 +1,17 @@
 import test from "ava";
-import Rsa from "../src";
+import { generateKey, encrypt, decrypt, importKeyFiles } from "../src";
 
 test("RSA: encrypt()/decrypt()", t => {
-    const message = "c213d39764dd60a0174a134ed8f7047dc7d14ff882a4355469a72732788ef3a3";
-    const testKey = Rsa.importKeyFiles('./samples/pub-test.pem', './samples/prv-test.pem');
-
-    const encryptDecrypt = (input: string): string => {
-      const encrypted = Rsa.encrypt(input, testKey.pub);
-      return Rsa.decrypt(encrypted, testKey.prv);
-    };
-
-    t.is(
-      encryptDecrypt(message),
-        message,
-      "Encrypted and then decrypted message should correspond to the original message."
-    );
-  });
+  const message =
+    "c213d39764dd60a0174a134ed8f7047dc7d14ff882a4355469a72732788ef3a3";
+  const key = generateKey();
+  const encryptDecrypt = (input: string): string => {
+    const encrypted = encrypt(input, key);
+    return decrypt(encrypted, key);
+  };
+  t.is(
+    encryptDecrypt(message),
+    message,
+    "Encrypted and then decrypted message should correspond to the original message."
+  );
+});
