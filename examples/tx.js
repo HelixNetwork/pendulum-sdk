cd57118f8e025d573b397ed6a4f3044af12cecdb
fsbbn <f.sophie.abben@gmail.com>
2019-04-26 16:56:03 +0200

    fully functional version

--- examples/tx.js
+++ examples/tx.js
@@ -8,13 +8,13 @@ const helix = Helix.composeAPI({
   provider: "http://localhost:14700"
 });
 
-const seed = "953c8169027a85415692cc05bd3a91f95c3be8e5c93c1d2b2e2c447b5ed082d2";
+const seed = "df36d3a5c687106be8c8880ce06117a302bd09fe88355cd4102b901ad9f76ec2";
 
 var recipientAddress1 =
   "064c7c7652a56055c3af2c620ee3a9daf4be3ad6cebaa8d5dd9ed8a8d7509ea1";
 
 var recipientAddress2 =
-  "16e092b38442a4887f510e15e58cefe024f212470df123a070c4db2f5ef4c6de";
+  "cf36dc1226a7e275641a2486a2aa2bf44347dac544d24079231597ee25b2cb9f";
 
 // Store the HBytes that are returned from prepareTransfers function
 var storedHBytes;
@@ -29,14 +29,14 @@ var transfer1 = {
 
 var transfer2 = {
   address: recipientAddress2,
-  value: 0,
+  value: 536561674354688,
   message: Converter.asciiToHBytes("abcd"),
   tag: "abcd123"
 };
 
 // Create bundle and return the HBytes of the prepared TXs
 helix
-  .prepareTransfers(seed, [transfer1, transfer2])
+  .prepareTransfers(seed, [transfer2])
   .then(function(HBytes) {
     storedHBytes = HBytes;
     // Finalize and broadcast the bundle to the node
