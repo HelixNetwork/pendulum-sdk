4784747b3934ccad7b5da09fdb73d1fce41b1bbc
fsbbn <f.sophie.abben@gmail.com>
2019-05-09 15:47:38 +0200

    pass not working unit tests

--- examples/tx.js
+++ examples/tx.js
@@ -8,27 +8,25 @@ const helix = Helix.composeAPI({
   provider: "http://localhost:14700"
 });
 
-const seed = "df36d3a5c687106be8c8880ce06117a302bd09fe88355cd4102b901ad9f76ec2";
+// Sender
+const senderSeed =
+  "df36d3a5c687106be8c8880ce06117a302bd09fe88355cd4102b901ad9f76ec2";
+const senderAddress =
+  "556a2431d03e57e92b7d4d4d37f98332fce5427d8167e16c0a5cfbe20899d261";
 
-var recipientAddress1 =
-  "064c7c7652a56055c3af2c620ee3a9daf4be3ad6cebaa8d5dd9ed8a8d7509ea1";
-
-var recipientAddress2 =
+// Receiver
+const receiverSeed =
+  "f6643fef386fb8e591dbbfde2d83e9176ecdd3af5b2699d65b8c8cb6ef721cad";
+const receiverAddress1 =
   "cf36dc1226a7e275641a2486a2aa2bf44347dac544d24079231597ee25b2cb9f";
+const receiverAddress2 =
+  "9477ec3c08c3e79a4fdcbc2a8fda9539b320b9b25dc9bc6cfca01cded875df0e";
 
 // Store the HBytes that are returned from prepareTransfers function
 var storedHBytes;
 
-// preparing transactions
-var transfer1 = {
-  address: recipientAddress1,
-  value: 0,
-  message: Converter.asciiToHBytes("abcd"),
-  tag: "abcd123"
-};
-
-var transfer2 = {
-  address: recipientAddress2,
+var transfer = {
+  address: receiverAddress1,
   value: 53,
   message: Converter.asciiToHBytes("abcd"),
   tag: "abcd123"
@@ -36,18 +34,17 @@ var transfer2 = {
 
 // Create bundle and return the HBytes of the prepared TXs
 helix
-  .prepareTransfers(seed, [transfer2])
+  .prepareTransfers(senderSeed, [transfer])
   .then(function(HBytes) {
     storedHBytes = HBytes;
     // Finalize and broadcast the bundle to the node
     return helix.sendHBytes(
       storedHBytes,
-      3 /*depth*/,
+      5 /*depth*/,
       2 /*minimum weight magnitude*/
     );
   })
-
-  .then(results => console.log(JSON.stringify(results)))
+  .then(results => console.log(results))
   .catch(err => {
     console.log(err);
   });
