2db18af1f6b05d94f71ee896950562819fbaf724
Cristina <cristina@transylvanialabs.com>
2019-07-22 09:44:16 +0300

    Removed comments

--- packages/core/sample/generateBundle.ts
+++ packages/core/sample/generateBundle.ts
@@ -24,89 +24,8 @@ const getNewAddress = createGetNewAddress(client, "lib");
 const helix = composeAPI({
   provider: "https://hlxtest.net:8087"
 });
-// let inputs: ReadonlyArray<any> = [
-//     {
-//         address: "1245e733c1abf577cff3056880f9963ae6170e4bd02c53dcea9db9501e8d52e1",
-//         keyIndex: 0,
-//         security: 2,
-//         balance: 4
-//     },
-//     {
-//         address: "df485155f326d19468056861b677277d48bfbda340bdc05c5c81333e9fc04d8c",
-//         keyIndex: 1,
-//         security: 2,
-//         balance: 4
-//     }
-// ];
-//
-// const transfers: ReadonlyArray<Transfer> = [
-//     {
-//         address: addChecksum("a".repeat(ADDRESS_BYTE_SIZE)),
-//         value: 3,
-//         tag: "aaaa",
-//         message: "abcd"
-//     }
-// ];
-//  const seed = 'abcd000000000000000000000000000000000000000000000000000000000000'
-
-// const now = () => 1522219924;
-// const prepareTransfers = createPrepareTransfers(undefined, now, "lib");
-// const prepareTransfersWithNetwork = createPrepareTransfers(
-//     createHttpClient(),
-//     now,
-//     "lib"
-// );
 
 async function generateBundle() {
-  // calculate addressesȘ
-  // const addresses: string[] = new Array<string>(3);
-  // const addr = await getNewAddress(seed, {index: 0, total: 3, security: 2});
-  // for (let i = 0; i < 3; i++) {
-  //     addresses[i] = addr[i];
-  // }
-  //
-  // inputs = inputs.map((value, i) => {
-  //     value.address = addresses[i];
-  //     return value;
-  // });
-  // const remainderAddress = addresses[2];
-  //
-  // const hbytes: ReadonlyArray<string> = await prepareTransfers(
-  //     seed,
-  //     transfers,
-  //     {
-  //         inputs,
-  //         remainderAddress
-  //     }
-  // );
-  // console.log(
-  //     "export const bundleHBytes: HBytes[] = " + JSON.stringify([hbytes].reverse()) + ";"
-  // );
-  // console.log("export const bundle: Transaction[] = ");
-  // console.log(
-  //     asTransactionObjects(
-  //         new Array<Transaction>(hbytes.length).map(tx => tx.hash)
-  //     )(hbytes).reverse()
-  // );
-  // inputs = inputs.slice(0, 1);
-  //
-  // const bundleWithValidSignature: ReadonlyArray<string> = await prepareTransfers(seed, transfers.slice(0, 1), {
-  //     inputs,
-  //     remainderAddress
-  // });
-  // console.log(
-  //     "export const bundleWithValidSignatureHBytes = " +
-  //     JSON.stringify([...bundleWithValidSignature].reverse()) +
-  //     ";"
-  // );
-  // console.log("export const bundleWithValidSignature = ");
-  // const bundleWithValidSig = asTransactionObjects(
-  //     new Array<Transaction>(bundleWithValidSignature.length).map(tx => tx.hash)
-  // )(bundleWithValidSignature).reverse();
-  // console.log(bundleWithValidSig);
-  // console.log("isValidBundle:" + isBundle(bundleWithValidSig));
-  //
-
   createAndPrintBundle(
     "export const bundle: Transaction[] = ",
     "export const bundleHBytes: HBytes[] = ",
